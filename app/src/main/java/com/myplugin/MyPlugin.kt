d87d84992149f304ec802ca24e319ec5eff8e273
User <user@github.com>
2026-08-04 10:19:53 +0800

    Convert to proper Aliucord plugin structure

---
+++ app/src/main/java/com/myplugin/MyPlugin.kt
@@ -0,0 +1,13 @@
+package com.myplugin
+
+import com.aliucord.entities.Plugin
+
+class MyPlugin : Plugin() {
+    override fun start() {
+        // Plugin initialization code
+    }
+
+    override fun stop() {
+        // Cleanup code
+    }
+}
